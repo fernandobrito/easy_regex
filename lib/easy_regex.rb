7146cbc3e1fdfcdcbf9ed5c3e09edade35649940
Fernando Brito <email@fernandobrito.com>
2014-02-26 15:42:19 -0300

    Add logger to the module

--- lib/easy_regex.rb
+++ lib/easy_regex.rb
@@ -1,2 +1,14 @@
 require "easy_regex/version"
 require "easy_regex/regex"
+require "easy_regex/compiler"
+
+require "logger"
+
+module EasyRegex
+  @@logger = Logger.new(STDOUT)
+  @@logger.level = Logger::INFO
+
+  def self.logger
+    @@logger
+  end
+end
