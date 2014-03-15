ec87bbce3bcd45c28a557de6deec6a45f091ee4f
David Guilherme <david.guilherme@outlook.com>
2014-03-14 23:08:30 -0300

    Fix method to_s

--- lib/easy_regex/automaton.rb
+++ lib/easy_regex/automaton.rb
@@ -5,15 +5,22 @@ module EasyRegex
 
     def initialize(fragment)
       @initial_state = fragment.start
+      @cache_string = nil
     end
 
     def to_s
-      @initial_state.inspect
+      if (@cache_string.nil?)
+        @cache_string = @initial_state.inspect
+      end
+
+      @cache_string
     end
   end
 
   class State
-    attr_accessor :char, :out1, :out2
+    attr_accessor :char, :out1, :out2, :id
+
+    @@count = 0
 
     ## OBS: Optional arguments with default value don't work in
     ## lower ruby versions than 2.0
@@ -21,6 +28,11 @@ module EasyRegex
       @char = char
       @out1 = out1
       @out2 = out2
+
+      @@count += 1
+      @id = @@count
+
+      @checked = false
     end
 
     def to_s
@@ -28,13 +40,24 @@ module EasyRegex
     end
 
     def inspect
-      output = "#{@char}"
-      out = "#{@out1}" if (@out1 && @out2 == nil)
-      out = "#{@out1} | #{out2}" if (@out1 && @out2)
+      output = ""
 
+      if (@char != :split)
+        output = "#{@char}[#{@id}]"
+      end
+
+      out = "#{@out1}" if ( @out1 && !@out1.checked? && @out2 == nil)
+      out = "#{@out1} | #{out2}" if (@out1 && !@out1.checked? && @out2 && !@out2.checked?)
+
+      output << "#{out}" if (@char == :split && out)
       output << " -> (#{out})" if out
 
+      @checked = true
       output
+    end
+
+    def checked?
+      @checked
     end
   end
 
