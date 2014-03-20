373afad3f4a8e719a10ba1a8084935a957059ce5
David Guilherme <david.guilherme@outlook.com>
2014-03-19 22:30:36 -0300

    Fix some problems with next list

--- lib/easy_regex/matcher.rb
+++ lib/easy_regex/matcher.rb
@@ -15,6 +15,7 @@ module EasyRegex
         EasyRegex::logger.debug("Matcher#match?: current: #{current_list} | next: #{next_list}")
 
         step(current_list, char, next_list)
+        EasyRegex::logger.debug("Matcher#match?: next_list #{next_list}")
         current_list, next_list = next_list, current_list
       end
 
@@ -23,13 +24,16 @@ module EasyRegex
 
   private
     def step(current_list, char, next_list)
+      next_list.clear
       current_list.each do |state|
+        EasyRegex::logger.debug("Matcher#step: next_list #{next_list}")
         if (state.char == char)
           EasyRegex::logger.debug("Matcher#step: #{char} match #{state.char}")
 
           add_state(next_list, state.out1)
         else
           EasyRegex::logger.debug("Matcher#step: #{char} does not match #{state.char}")
+          EasyRegex::logger.debug("Matcher#step: next_list #{next_list}")
         end
       end
     end
